90c42d3c006c07fb86d0269fe9ba6fc77976024e
KISHAN JAWALE <kishanjawale10@gmail.com>
2022-03-30 12:08:51 +0530

    Update Mainfile.C

--- Mainfile.C
+++ Mainfile.C
@@ -1,13 +1,19 @@
 #include<stdio.h>
 
+//////////////////////////////////////////////////////////////////////////////////////
+//Description:This  application program Demonstrate  the concept of extern storage class in C Programming
+//We can use this concept of extern storage class when we have to divide our code in two or more files.
+//@Author:Kishan Jawale
+//Date: 30/03/2022
+/////////////////////////////////////////////////////////////////////////////////////////
+ 
 extern int iValue1; //Accessing the value of iValue1 from  Extern.c file
 extern int iValue2; //Accessing the value of iValue2 from  Extern.c file
  int main()
  {
-     
      int iAns= iValue1+iValue2;
 
-    printf("Addition of A and  B is %d",iAns);
+     printf("Addition of A and  B is %d",iAns);
 
      return 0;
 
